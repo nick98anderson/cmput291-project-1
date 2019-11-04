a4ed507f2f70f00dc686ef8fabb4946a0ddc03a8
Nick Anderson <47461646+nick98anderson@users.noreply.github.com>
2019-11-04 03:46:01 -0700

    fixed find_car_owner and updated logout

--- miniproject1.py
+++ miniproject1.py
@@ -2,6 +2,8 @@ import sqlite3
 import sys
 import datetime
 from random import randrange
+
+
 '''Resources for SQLite in python and implementing user logins
 https://eclass.srv.ualberta.ca/pluginfile.php/5149362/mod_label/intro/SQLite-in-Python-1.pdf -- BEST
 https://eclass.srv.ualberta.ca/mod/page/view.php?id=3659763 -- Assignment Spec
@@ -12,7 +14,11 @@ db = sys.argv[1]
 conn = sqlite3.connect(db)
 cursor = conn.cursor()
 
+def double_quote(word):
+    return "'%s'" % word
+
 def main():
+    print("***WELCOME****")
     userType = login()
     
 
@@ -38,9 +44,9 @@ def login():
 
 
 def getUser():
-    username = input("User ID: ")
-    pwd = input("Password: ")
-    cursor.execute("SELECT utype, uid, city FROM users WHERE uid = ? AND pwd = ?", (username, pwd))
+    username = raw_input("User ID: ")
+    pwd = raw_input("Password: ")
+    cursor.execute("SELECT utype FROM users WHERE uid = ? AND pwd = ?", (username, pwd))
     user = cursor.fetchall()
     return user
 
@@ -57,7 +63,7 @@ def officer_prompt():
             find_car_owner()
             display_officer_options()
         elif option == 2:
-            break
+            logout()
         else:
             print("****ERROR***** invalid option please try again")
             display_officer_options()
@@ -89,8 +95,7 @@ def agent_prompt():
             get_driver_abstract()
             display_agent_options()
         elif option == 6:
-            break
-            #logout()
+            logout()
         else:
             print("****ERROR***** invalid option please try again")
             display_agent_options()
@@ -133,21 +138,25 @@ def get_driver_abstract():
     pass
 
 def logout():
-    pass
+    print("Loging out....")
+    main()
 
+
+#Todo-> Check for valid date, string matching
 def issue_ticket():
+    
     while True:
-    regno = int(input("Enter a registration number: "))
-    cursor.execute("SELECT regno, vin, fname, lname FROM registrations WHERE regno = ?;", (regno,))
-    regInfo = cursor.fetchall()
-    print(regInfo)
-    conn.commit()
-    if regInfo[0][0] == regno:
-        break
-    else:
-        option = raw_input('Registration number not found would you like to try again (y/n): ')
-        if option == 'n':
-                return
+        regno = int(input("Enter a registration number: "))
+        cursor.execute("SELECT regno, vin, fname, lname FROM registrations WHERE regno = ?;", (regno,))
+        regInfo = cursor.fetchall()
+        conn.commit()
+
+        if regInfo == []:
+            option = raw_input('Registration number not found would you like to try again (y/n): ')
+            if option == 'n':
+                return 
+        elif regInfo[0][0] == regno:
+            break
     
     vin = regInfo[0][1]
     fname = regInfo[0][2]
@@ -202,7 +211,7 @@ def issue_ticket():
 
 
 def find_car_owner():
-    selectQuery = "SELECT v.vin, v.make, v.model, v.year, v.color, r.plate FROM vehicles v, registrations r WHERE "
+    selectQuery = "SELECT v.make, v.model, v.year, v.color, r.plate, regdate, expiry, fname, lname FROM vehicles v LEFT JOIN registrations r ON r.vin = v.vin WHERE "
     carDetails = ['make', 'model', 'year', 'color', 'plate']
     userSelections = []
     userValues = []
@@ -217,33 +226,39 @@ def find_car_owner():
         print("ERROR, you must enter at least one of the fields")
         return
 
-    
-
     for i in range(0, len(userSelections)):
-        if userSelections[i] == 'year':
-            selectQuery += 'v.'+ userSelections[i] + '=' + userValues[i] + ' AND '
-        elif userSelections[i] == 'plate':
-            selectQuery += 'r.'+userSelections[i] + '=' + double_quote(userValues[i]) + ' AND '
+        if i == len(userSelections) - 1:
+            if userSelections[i] == 'year':
+                selectQuery += 'v.'+ userSelections[i] + '=' + userValues[i]
+            elif userSelections[i] == 'plate':
+                selectQuery += 'r.'+userSelections[i] + '=' + double_quote(userValues[i])
+            else:
+                selectQuery += 'v.' + userSelections[i] + ' LIKE ' + double_quote(userValues[i])
         else:
-            selectQuery += 'v.' + userSelections[i] + ' LIKE ' + double_quote(userValues[i]) + ' AND '
-        
-    selectQuery = selectQuery + ' r.vin = v.vin'
+            if userSelections[i] == 'year':
+                selectQuery += 'v.'+ userSelections[i] + '=' + userValues[i] + ' AND '
+            elif userSelections[i] == 'plate':
+                selectQuery += 'r.'+userSelections[i] + '=' + double_quote(userValues[i]) + ' AND '
+            else:
+                selectQuery += 'v.' + userSelections[i] + ' LIKE ' + double_quote(userValues[i]) + ' AND '
     
-    
+    selectQuery += " GROUP BY v.vin"
+
     cursor.execute(selectQuery)
     fetched = cursor.fetchall()
+
 
     if len(fetched) == 0:
         print('No results found')
 
     elif len(fetched) >= 4:
-        formated_row = '{:<10} {:>6} {:>6} {:>6} {:>6}' 
+        formated_row = '{:<10} {:>6} {:>6} {:>6} {:^10}' 
         i = 1
         for row in fetched:
             if i == 1:
                 print("    "+formated_row.format("Make", "Model", "Year", "Color", "Plate"))
             
-            print(str(i)+ "   " + formated_row.format(row[1], row[2], row[3], row[4], row[5]))
+            print(str(i)+ "   " + formated_row.format(row[0], row[1], row[2], row[3], row[4]))
             i = i + 1
 
         selectionRow = int(raw_input("Select row from vehicles for more information: "))
@@ -254,31 +269,32 @@ def find_car_owner():
         
         selection = fetched[selectionRow-1]
 
-        vin = str(selection[0])
-        make = str(selection[1])
-        model = str(selection[2])
-        year = str(selection[3])
-        color = str(selection[4])
-        plate = str(selection[5])
-        vinQuery = """SELECT make, model, year, color, plate,regdate, expiry, fname, lname FROM vehicles, registrations 
-                    WHERE make LIKE ? AND model LIKE ? AND year = ? AND color LIKE ? AND registrations.vin = ? ORDER BY regdate DESC LIMIT 1"""
-        
-        cursor.execute(vinQuery, (make,model,year,color,vin))
-        fetched = cursor.fetchall()
-        conn.commit
+        make = str(selection[0])
+        model = str(selection[1])
+        year = str(selection[2])
+        color = str(selection[3])
+        plate = str(selection[4])
         formated_row = '{:<10} {:^10} {:^10} {:^10} {:^10} {:^15} {:^15} {:^15} {:^15}'
-        print(formated_row.format("Make", "Model", "Year", "Color", "Plate", "Regdate", "Expiry", "First name", "Last name")) 
-        for row in fetched:
-            print(formated_row.format(*row))
-        
+
+        if plate == "None":
+            print(formated_row.format("Make", "Model", "Year", "Color", "Plate","Regdate","Expiry Date","First name", "Last name"))
+            print(formated_row.format(make, model, year, color, plate, "None", "None", "None", "None"))
+
+        else:
+            selectQuery = """SELECT v.make, v.model, v.year, v.color, r.plate, r.regdate, r.expiry, r.fname, r.lname FROM vehicles v 
+            LEFT JOIN registrations r ON v.vin = r.vin WHERE v.make like ? AND v.model like ? AND v.year = ? AND v.color LIKE ? AND r.plate = ?
+            ORDER BY r.regdate DESC LIMIT 1"""
+            cursor.execute(selectQuery, (make, model, year, color, plate))
+            fetched = cursor.fetchall() 
+            print(formated_row.format("Make", "Model", "Year", "Color", "Plate","Regdate","Expiry Date","First name", "Last name"))
+            for row in fetched:
+                print(formated_row.format(*row))
+
     else:
-        e = selectQuery.split("WHERE", 1)[1]
-        vinQuery = "SELECT v.make, v.model, v.year, v.color, r.plate, r.regdate, r.expiry, r.fname, r.lname FROM vehicles v, registrations r WHERE" + e + " AND r.vin = v.vin ORDER BY r.regdate DESC LIMIT 1"
-        cursor.execute(vinQuery)
-        fetched = cursor.fetchall()
-        conn.commit
-        formated_row = '{:<10} {:<10} {:<10} {:<10} {:>10} {:<15} {:<15} {:<15} {:<15}'
-        print(formated_row.format("Make", "Model", "Year", "Color","Plate", "Regdate", "Expiry", "First name", "Last name")) 
+        formated_row = '{:<10} {:^10} {:^10} {:^10} {:^10} {:^15} {:^15} {:^15} {:^15}'
+        selectQuery += "AND r.regdate = (SELECT MAX(regdate) FROM registrations WHERE vin = v.vin)"
+        print(selectQuery)
+        print(formated_row.format("Make", "Model", "Year", "Color", "Plate","Regdate","Expiry Date","First name", "Last name"))
         for row in fetched:
             print(formated_row.format(*row))
 
@@ -289,5 +305,6 @@ def find_car_owner():
 main()
 
 conn.close()
+
 
 
